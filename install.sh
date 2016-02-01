f85160a6a87bac6e94bc7e6e8b684f8137715771
a <a>
2016-02-01 11:59:01 +0800

    Fix multiline echo

--- install.sh
+++ install.sh
@@ -2,17 +2,19 @@
 
 echo "source ~/.confJuang/bashrc" > ~/.bashrc
 echo "source ~/.confJuang/tmux.conf" > ~/.tmux.conf
-echo "$include /etc/inputrc
-set completion-ignore-case On" > ~/.inputrc
+echo "\$include ~/.confJuang/inputrc" > ~/.inputrc
 
+# gitconfig
 echo "Following are Git settings: "
 read -p "What's your name? " name
 read -p "And your email? " email
-echo "[user]
+cat > ~/.gitconfig <<- EOM
+[user]
     email = $email
     name = $name
 [include]
-    path = ~/.confJuang/gitconfig" > ~/.gitconfig
+    path = ~/.confJuang/gitconfig
+EOM
 
 echo "ConfJuang Installed Successfully! Enjoy~"
 echo "Any problem please contact Yi-Lin Juang (frankyjuang@gmail.com)"
