c7fad3cd21186e1caa7c5e6e499f06e578755ad5
Juang, Yi-Lin <frankyjuang@gmail.com>
2016-02-01 11:31:44 +0800

    Add git alias

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-echo 'source ~/.confJuang/bashrc' > ~/.bashrc
-echo 'source ~/.confJuang/tmux.conf' > ~/.tmux.conf
-echo '$include /etc/inputrc
-set completion-ignore-case On' > ~/.inputrc
+echo "source ~/.confJuang/bashrc" > ~/.bashrc
+echo "source ~/.confJuang/tmux.conf" > ~/.tmux.conf
+echo "$include /etc/inputrc
+set completion-ignore-case On" > ~/.inputrc
+
+echo "Following are Git settings: "
+read -p "What's your name? " name
+read -p "And your email? " email
+echo "[user]
+    email = $email
+    name = $name
+[include]
+    path = ~/.confJuang/gitconfig" > ~/.gitconfig
 
 echo "ConfJuang Installed Successfully! Enjoy~"
 echo "Any problem please contact Yi-Lin Juang (frankyjuang@gmail.com)"
